f6c610b59e1eeca83bd103a5a01c047e05a590f5
Gaurav Raj <gauravraj0811@gmail.com>
2024-12-19 18:24:50 +0530

    fixing bug

--- drizzle.config.js
+++ drizzle.config.js
@@ -3,6 +3,6 @@ export default {
   schema: "./utils/schema.js",
   dialect: "postgresql",
   dbCredentials: {
-    url: "postgresql://accounts:fGEscg5qT8aU@ep-raspy-star-a5f0mi2q.us-east-2.aws.neon.tech/ai-interview-mocker?sslmode=require",
+    url: NEXT_PUBLIC_DRIZZLE_DB_URL,
   },
 };
