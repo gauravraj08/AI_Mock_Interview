d201c08efc0293426e8b10974431ab8ca8840c42
Gaurav Raj <136689227+gauravraj08@users.noreply.github.com>
2025-03-30 02:23:47 +0530

    Update drizzle.config.js

--- drizzle.config.js
+++ drizzle.config.js
@@ -3,6 +3,6 @@ export default {
   schema: "./utils/schema.js",
   dialect: "postgresql",
   dbCredentials: {
-    url: "postgresql://accounts:fGEscg5qT8aU@ep-raspy-star-a5f0mi2q.us-east-2.aws.neon.tech/ai-interview-mocker?sslmode=require",
+    url: "postgresql://ai-interview-mocker_owner:PK2Dh0oyIQkp@ep-raspy-star-a5f0mi2q-pooler.us-east-2.aws.neon.tech/ai-interview-mocker?sslmode=require",
   },
 };
